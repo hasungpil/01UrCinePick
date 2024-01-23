011021af17e311102b46421535073b70ad8a327f
rebehayan <rebehayan@gmail.com>
2024-01-23 16:13:36 +0900

    검색페이지 헤더추가

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -4,7 +4,7 @@ import Heading from "../components/Common/Heading";
 const Search = () => {
   return (
     <>
-      <Heading tag="h2" className={"sub-title"} />
+      <Heading tag="h2" className={"sub-title"} subtitle="MOST POPULAR MOVIES" maintitle="MOVIE SEARCHING RESULTS" />
     </>
   );
 };
